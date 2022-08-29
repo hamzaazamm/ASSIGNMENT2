9c25b22031369237cdabe0928504e8c5e9086ee0
Hamza <111911421+hamzaazamm@users.noreply.github.com>
2022-08-29 17:57:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+var name = "hamza";
+name = "saleem";;
+
+
+var name = "ali bhai";
+var nationality = 'pakistani';
+var message = "What is your father's number";
+
+
+
+var age = 15;
+var weight = 15.5;
+var weights = weight + 20;
+var orignal = 12  ;
+
+
+var Paid = true;
+var Passed = false;
+
+
+
+var name = null;
+var nationality = "pakistani";
+nationality = null;
+
+
+typeof "hiii";
+var a = 55;
+typeof c;
+typeof true; 
+
+
+var a = 6;
+var b = 10;
+var c = 0;
+c = a + b;
+alert(a);
+console.log(c); //16
+
+
+var age = 18;
+++age;
+alert(age); //19
+--age;
+alert(age); //18
+
+
+var age = 52;
+age++;
+alert(age);//53
+age--;
+alert(age); //52
+
+
+
+var a =7;
+var b =2;
+var c = a++ - b; 
+alert(a);//7
+alert(b);//2
+alert(c); //6
+
+
+var firstName = "hamza";
+var lastName = "azam";
+var fullName = firstName + " " + lastName;
+alert(fullName);//hamza azam
+
+
+
+var a = "6" + 2; //62
+var b = 3 + "6";  //36
+var c = "Hello " + 2; //Hello 2
+var d = 2 + "Hello ";  //2Hello
